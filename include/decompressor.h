f4965452849af41a16b22bdc0808bb5d205e1567
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-10-26 08:20:52 -0300

    Iniciando o desenvolvimento da descompactação LZW

--- include/decompressor.h
+++ include/decompressor.h
@@ -1,1 +1,23 @@
- 
+#ifndef DECOMPRESSOR_H
+#define DECOMPRESSOR_H
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Decompressor
+{
+    const unsigned int tamanho_maximo = 32767;
+    unordered_map<unsigned int, string> simbolos;
+    string simbolo_anterior;
+    unsigned int codigo;
+    unsigned int proximo_codigo = 257;
+
+    public:
+    Decompressor();
+    ~Decompressor();
+
+
+};
+
+#endif 
--- src/decompressor.cpp
+++ src/decompressor.cpp
@@ -1,1 +1,37 @@
- 
+#include "../include/decompressor.h" 
+
+Decompressor::Decompressor()
+{
+    for (unsigned int i = 0; i < 256; i++)
+    {
+        simbolos[i] = string(1, i);
+    }
+
+    while(true)
+    {
+        /**
+         * Caso em que o código do simbolo descoberto está no dicionário
+         **/
+        if(simbolos.find(codigo) != simbolos.end())
+        {
+            simbolos[codigo] = simbolo_anterior + simbolo_anterior[0];
+        }
+
+        /**
+         * Caso em que o código do simbolo descoberto não está no dicionário
+         **/
+        if (simbolo_anterior.size() && proximo_codigo <= tamanho_maximo)
+        {
+            simbolos[proximo_codigo++] = simbolo_anterior + simbolos[codigo][0];
+        }
+        simbolo_anterior = simbolos[codigo];
+
+    }
+
+}
+
+Decompressor::~Decompressor()
+{
+
+}
+

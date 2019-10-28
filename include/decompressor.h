13fb64ac7b622508effb037c1b9058df70656dda
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-10-28 08:46:14 -0300

    Refatorando classe Decompressor

--- include/decompressor.h
+++ include/decompressor.h
@@ -4,21 +4,23 @@
 #define EXTENSION_ERROR "Extensão de arquivo compactado não reconhecida"
 #define EXTENSION "cmp"
 #define DOT "."
+#define BITS 14
 
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
+#include "file-manager.h"
+#include "../src/file-manager.cpp"
 
 using namespace std;
 
 class Decompressor
 {
-    const unsigned int tamanho_maximo = 32767;
+    const unsigned int tamanho_maximo = (2 << BITS) - 1;
     unordered_map<unsigned int, string> simbolos;
     
-    ifstream* arquivo_compactado;    
-    ofstream* arquivo_descompactado;
-    
+    string nome_arquivo_compactado;
+    string nome_arquivo_descompactado;
     string simbolo_anterior;
     unsigned int codigo;
     unsigned int proximo_codigo = 257;
--- src/decompressor.cpp
+++ src/decompressor.cpp
@@ -2,49 +2,51 @@
 
 Decompressor::Decompressor(string filename)
 {
+    cout << tamanho_maximo << endl;
+    nome_arquivo_compactado = filename;
     int posicao = filename.find(DOT);
-    string nome_final = filename.substr(0, posicao);
+    nome_arquivo_descompactado = filename.substr(0, posicao);
     string extensao = filename.substr(posicao + 1);
     if (extensao != EXTENSION)
     {
         cerr << EXTENSION_ERROR;
         exit(EXIT_FAILURE);
     }
-    arquivo_compactado = new ifstream(filename, ifstream::in);
-    arquivo_descompactado = new ofstream(nome_final, ofstream::out);
 }
 
 void Decompressor::lzwDecompression()
 {
+    istream* arquivo = new ifstream(nome_arquivo_compactado);
+    InputStream<istream> arquivo_compactado(*arquivo, tamanho_maximo);    
+    ostream* arquivo_final = new ofstream(nome_arquivo_descompactado);
+    OutputStream<ostream> arquivo_descompactado (*arquivo_final);
     for (unsigned int i = 0; i < 256; i++)
         simbolos[i] = string(1, i);
     
-    while(proximo_codigo < 300) // condição de loop errada
+    while(arquivo_compactado >> codigo)
     {
         /**
-         * Caso em que o código do simbolo descoberto está no dicionário
+         * Caso em que o código do simbolo descoberto não está no dicionário
          **/
-        if(simbolos.find(codigo) != simbolos.end())
+        if(simbolos.find(codigo) == simbolos.end())
         {
             simbolos[codigo] = simbolo_anterior + simbolo_anterior[0];
         }
-
+        
+        arquivo_descompactado << simbolos[codigo];
         /**
-         * Caso em que o código do simbolo descoberto não está no dicionário
+         * Caso em que o código do simbolo descoberto esteja muito a frente no dicionário
          **/
         if (simbolo_anterior.size() && proximo_codigo <= tamanho_maximo)
         {
             simbolos[proximo_codigo++] = simbolo_anterior + simbolos[codigo][0];
         }
         simbolo_anterior = simbolos[codigo];
-        proximo_codigo++;
     }
 }
 
 Decompressor::~Decompressor()
 {
-    arquivo_compactado->close();
-    arquivo_descompactado->close();
     simbolos.clear();
 }
 

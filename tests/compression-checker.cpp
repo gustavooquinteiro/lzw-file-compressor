c9cdcf0ddd458b19846acd16a096b379cbadc8f5
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-11-16 20:23:12 -0300

    Atualizando informações mostradas no checker de compressão

--- tests/compression-checker.cpp
+++ tests/compression-checker.cpp
@@ -2,12 +2,51 @@
 #include <fstream>
 using namespace std;
 
+bool kilo;
+bool mega;
+bool giga;
+
+void reset()
+{
+    kilo = false;
+    mega = false;
+    giga = false;
+}
+
+double converter(size_t tamanho)
+{
+    reset();
+    double valor = 0; 
+    if (tamanho >= 1024 && tamanho < 1048576){
+        kilo = true;
+        valor = tamanho * 0.00097656;
+    } else if (tamanho >= 1048576 && tamanho < 1073741824){
+        mega = true;
+        valor = tamanho * 0.00000095;
+    } else if (tamanho >= 1073741824) {
+        giga = true;
+        valor = tamanho * 0.00000000093;
+    } else{
+        valor = tamanho;
+    }
+    return valor;
+}
+
+
 int main(int argc, char**argv)
 {
     ifstream original(argv[1], ifstream::ate | ifstream::binary);
     ifstream compressed(argv[2], ifstream::ate | ifstream::binary);
+    
     size_t original_size = original.tellg();
     size_t compressed_size = compressed.tellg();
+    
+    cout << "_______________________________________________________________" << endl;
+    cout <<"|Nome do arquivo   |   Tamanho do arquivo" << endl;
+    cout <<"| "<< argv[1] << " | " << converter(original_size) << ((kilo)?"Kib":(mega)?"Mib": (giga)?"Gib":"b") <<endl;
+    cout <<"| "<< argv[2] << " | " << converter(compressed_size) << ((kilo)?"Kib":(mega)?"Mib": (giga)?"Gib":"b")<< endl;
+    cout << "_______________________________________________________________" << endl;
+    
     if (original_size > compressed_size)
     {
         int taxa =  ((original_size - compressed_size)*100) / original_size;
@@ -17,6 +56,6 @@ int main(int argc, char**argv)
         cout << "Não houve compactação entre os arquivos" << endl;
     } else
     {
-        cout << "Arquivo compactado maior que o original" << endl;
+        cout << "Arquivo compactado maior que o original: Aumento de  " << converter(compressed_size - original_size) << ((kilo)?"Kib":(mega)?"Mib": (giga)?"Gib":"b") << endl;
     }
 }
